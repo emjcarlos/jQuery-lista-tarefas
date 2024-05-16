c7f6a8a7ec2adde51f497cf39459a721b9a2cc86
Carlos Eduardo Martins Joao <emj17carlos@gmail.com>
2024-05-15 23:56:03 -0300

    novo

--- main.js
+++ main.js
@@ -2,22 +2,28 @@ $(document).ready(function () {
   $("form").on("submit", function (e) {
     e.preventDefault();
 
-    const enderecoTarefaNova = $("#endereco-terefa-nova").val();
+    const enderecoTarefaNova = $("#inputAdiciona").val();
 
     const novaTarefa = $("<li></li>").appendTo("ul");
 
-    $("<div class='texto-tarefa'></div>")
-      .append(
-        `<a onclick='concluirTeste()' id="teste">${enderecoTarefaNova}</a>`
-      )
+    let arrayLista = [];
 
+    $("<li class='texto-tarefa'></li>")
+      .append(`<a id="teste">${enderecoTarefaNova}</a>`)
       .appendTo(novaTarefa);
 
-    function concluirTeste() {
-      let adicionar = document.getElementById("teste");
-      adicionar.classList.add("seletor");
-    }
+    $("#teste").click(function () {
+      $(this).attr("class", "seletor");
+    });
 
-    $("#endereco-terefa-nova").val("");
+    $("#teste").dblclick(function () {
+      $(this).attr("class", "seletor1");
+    });
   });
+
+  //  function addTarefa() {
+  //  arrayLista.push(input);
+  // }
+
+  $("#inputAdiciona").val(" ");
 });
