17a18457207ea5382efb9b244259f068849936a5
Carlos Eduardo Martins Joao <emj17carlos@gmail.com>
2024-05-20 19:46:09 -0300

    exercício jquery pronto

--- main.js
+++ main.js
@@ -2,29 +2,18 @@ $(document).ready(function () {
   $("form").on("submit", function (e) {
     e.preventDefault();
 
-    let array = [];
-
     const enderecoTarefaNova = $("#inputAdiciona").val();
 
     const novaTarefa = $("<li></li>").appendTo("ul");
 
     $("<div class='texto-tarefa'></div>")
-      .append(`<a id='teste'>${enderecoTarefaNova}</a>`)
+      .append(`<a >${enderecoTarefaNova}</a>`)
       .appendTo(novaTarefa);
 
-    $("#teste").click(function () {
-      $("#teste").attr("class", "seletor");
-    });
-
-    $("#teste").dblclick(function () {
-      $("#teste").attr("class", "seletor1");
-    });
-
-    function valorInput() {
-      array.push(enderecoTarefaNova);
-      console.log(valorInput());
-    }
-
     $("#inputAdiciona").val("");
+
+    $("ul").on("click", "li", function (e) {
+      $(this).toggleClass("seletor");
+    });
   });
 });
